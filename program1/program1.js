449b2b4f1e5d146211dd71ddd5539709ec81cd18
preethi.dronamraju <dronamraju.preethi93@gmail.com>
2018-10-30 09:19:56 -0500

    inital code

--- program1/program1.js
+++ program1/program1.js
@@ -10,9 +10,46 @@ if(x%2 ==0)
   console.log("odd");
 
 //5 programs
+//if else
+var marks = 100;
+if(marks > 75){
+ console.log("distinction");
+}else{
+ console.log("pass");
+}
+
+
+//if
+var age =30;
+if(age<18){
+ console.log("not allowed");
+ }
+
+ //multiple ifelse
+ var marks =100;
+ if(marks>95){
+ console.log("a grade");
+ }else if(marks>85 && marks<70){
+ console.log("b grade");
+ }else{
+ console.log("c grade");
+ }
 
 
 //for loop, while loop
 
 //3 progams
 
+//for loop
+var x;
+for(x=10;x <= 100;x++){
+console.log(x);
+}
+
+//while loop
+var profit =0;
+while(profit<30){
+console.log(profit);
+profit++
+}
+console.log("no profit anymore");
