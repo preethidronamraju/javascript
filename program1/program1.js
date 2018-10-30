f68e944dd46147d0f4d6faef809b0ff6d495d9f1
preethi.dronamraju <dronamraju.preethi93@gmail.com>
2018-10-29 21:19:04 -0500

    inital code

---
+++ program1/program1.js
@@ -0,0 +1,18 @@
+console.log("preethi");
+
+//control flow statements
+//if, if-else, multiple if else blocks
+
+var x = 21;
+if(x%2 ==0)
+ console.log("even")
+ else
+  console.log("odd");
+
+//5 programs
+
+
+//for loop, while loop
+
+//3 progams
+
